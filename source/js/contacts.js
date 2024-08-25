75a99f6a3b46d5b334ed3437c976483ffb333e0c
Andrey <byketch@yandex.ru>
2024-08-25 11:17:49 +0300

    добавление видео с rutube

--- source/js/contacts.js
+++ source/js/contacts.js
@@ -13,30 +13,30 @@ document.addEventListener('DOMContentLoaded', function() {
    
        //Поп-ап видео
    
-    //    let clinicVideoArea = document.querySelector('.clinics__card-video');
-    //    let popUpVideo = document.querySelector('.popup-video');
-    //    let popUpVideoOverlay = document.querySelector('.popup-video__overlay');
-    //    let popUpVideoClose = document.querySelector('.popup-video__close');
-    //    let popUpVideoIframe = document.getElementById('contacts-video');
-    //    let video_url = popUpVideoIframe.getAttribute('src');
+       let clinicVideoArea = document.querySelector('.clinics__card-video');
+       let popUpVideo = document.querySelector('.popup-video');
+       let popUpVideoOverlay = document.querySelector('.popup-video__overlay');
+       let popUpVideoClose = document.querySelector('.popup-video__close');
+       let popUpVideoIframe = document.getElementById('contacts-video');
+       let video_url = popUpVideoIframe.getAttribute('src');
    
-    //    clinicVideoArea.addEventListener('click', function() {
-    //        popUpVideo.style.visibility = 'visible';
-    //        document.body.classList.add('body-overflow');
-    //        popUpVideoIframe.setAttribute('src', video_url);
-    //    });
+       clinicVideoArea.addEventListener('click', function() {
+           popUpVideo.style.visibility = 'visible';
+           document.body.classList.add('body-overflow');
+           popUpVideoIframe.setAttribute('src', video_url);
+       });
    
-    //    popUpVideoClose.addEventListener('click', function() {
-    //        popUpVideo.style.visibility = 'hidden';
-    //        document.body.classList.remove('body-overflow');
-    //        popUpVideoIframe.setAttribute('src', '');
-    //    });
+       popUpVideoClose.addEventListener('click', function() {
+           popUpVideo.style.visibility = 'hidden';
+           document.body.classList.remove('body-overflow');
+           popUpVideoIframe.setAttribute('src', '');
+       });
    
-    //    popUpVideoOverlay.addEventListener('click', function() {
-    //        popUpVideo.style.visibility = 'hidden';
-    //        document.body.classList.remove('body-overflow');
-    //        popUpVideoIframe.setAttribute('src', '');
-    //    });
+       popUpVideoOverlay.addEventListener('click', function() {
+           popUpVideo.style.visibility = 'hidden';
+           document.body.classList.remove('body-overflow');
+           popUpVideoIframe.setAttribute('src', '');
+       });
 
 
 
