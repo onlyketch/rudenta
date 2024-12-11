eb428df284d1fe05493c5ab7937ba05be608a9c9
Andrey <byketch@yandex.ru>
2024-12-11 16:14:18 +0300

    разработка страницы отзывов

--- source/js/main.js
+++ source/js/main.js
@@ -240,6 +240,245 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 400); 
     });
 
+    /************ Filters left side of reviews Start  *************/ 
+
+    let reviewsFilters = document.querySelector('.reviews__filters');
+    let reviewsFiltersResetBtn = document.querySelector('.reviews__filters-reset');
+    let reviewsFiltersItemClinics = document.querySelectorAll('.reviews__filters-item-clinic');
+    let reviewsFiltersItemDoc = document.querySelector('.doctor');
+    let reviewsFiltersItemServ = document.querySelector('.service');
+    let reviewsFiltersValueDoc = document.getElementById('doc-val');
+    let reviewsFiltersValueServ = document.getElementById('service-val');  
+    let reviewsFiltersDropDownDoc = document.querySelector('.doc');
+    let reviewsFiltersDropDownServ = document.querySelector('.serv');
+    let reviewsFiltersDropDownItems = document.querySelectorAll('.reviews__filters-item-dropdown-item');
+    let reviewsFiltersDropDownCheckboxesGroups = document.querySelectorAll('.reviews__filters-item-dropdown-checkboxes');
+    let selectedCheckboxesArray = [];
+
+    
+
+    if (reviewsFilters !== null) {
+
+        for (let i = 0; i < reviewsFiltersItemClinics.length; i++) {
+            reviewsFiltersItemClinics[i].addEventListener('click', function() {
+                if (!this.classList.contains('active')) {
+                    for (let item of reviewsFiltersItemClinics) {
+                        item.classList.remove('active');
+                    }
+                    this.classList.add('active');
+                }
+            });
+        };
+
+        reviewsFiltersItemDoc.addEventListener('click', function() {
+            reviewsFiltersDropDownDoc.classList.toggle('show');
+        });
+
+        reviewsFiltersItemServ.addEventListener('click', function() {
+            reviewsFiltersDropDownServ.classList.toggle('show');
+        });
+
+        document.addEventListener('click', function(e) {
+            const withinBoundaries = e.composedPath().includes(reviewsFiltersDropDownDoc);
+            const withinBtn = e.composedPath().includes(reviewsFiltersItemDoc);
+         
+            if ( !withinBoundaries && !withinBtn ) {
+                if (reviewsFiltersDropDownDoc.classList.contains('show')) {
+                    reviewsFiltersDropDownDoc.classList.remove('show');
+                }
+            }
+        });
+
+        document.addEventListener('click', function(e) {
+            const withinBoundaries = e.composedPath().includes(reviewsFiltersDropDownServ);
+            const withinBtn = e.composedPath().includes(reviewsFiltersItemServ);
+         
+            if ( !withinBoundaries && !withinBtn ) {
+                if (reviewsFiltersDropDownServ.classList.contains('show')) {
+                    reviewsFiltersDropDownServ.classList.remove('show');
+                }
+            }
+        });
+
+        for (let i = 0; i < reviewsFiltersDropDownItems.length; i++) {
+            reviewsFiltersDropDownItems[i].addEventListener('click', function() {
+                for (let item of reviewsFiltersDropDownItems) {
+                    if (item.classList.contains('choosed')) {
+                        item.classList.remove('choosed');
+                    }
+                }
+                reviewsFiltersDropDownItems[i].classList.add('choosed');
+                reviewsFiltersValueDoc.value = this.querySelector('.reviews__filters-item-dropdown-docname').textContent;
+            });
+        };
+
+
+        for (let i = 0; i < reviewsFiltersDropDownCheckboxesGroups.length; i ++) {
+            let checkboxMain = reviewsFiltersDropDownCheckboxesGroups[i].querySelector('.checkbox-main');
+            let groupCheckboxes = reviewsFiltersDropDownCheckboxesGroups[i].querySelectorAll('.checkbox-group-item');
+            checkboxMain.addEventListener('click', function() {
+                for (let i = 0; i < groupCheckboxes.length; i++) {
+                    groupCheckboxes[i].checked = this.checked;
+                    groupCheckboxes[i].dispatchEvent(new Event('change'));
+                }
+            });
+
+            for (let i = 0; i < groupCheckboxes.length; i++) {
+                groupCheckboxes[i].addEventListener('change', function() {
+                    if (groupCheckboxes[i].checked) {
+                        selectedCheckboxesArray.push(groupCheckboxes[i].value);
+                    } else {
+                        selectedCheckboxesArray = selectedCheckboxesArray.filter(function(item) {
+                            if (item !== groupCheckboxes[i].value) {
+                                return true;
+                            } 
+                        });
+                    }
+                    let selectedCheckboxesValues = selectedCheckboxesArray.join(', ');
+                    reviewsFiltersValueServ.value = selectedCheckboxesValues; 
+                });
+            }
+        };
+
+        reviewsFiltersResetBtn.addEventListener('click', function() {
+            reviewsFiltersValueDoc.value = '';
+            reviewsFiltersValueServ.value = '';
+            
+            for (let i = 0; i < reviewsFiltersDropDownCheckboxesGroups.length; i++) {
+                let checkboxMain = reviewsFiltersDropDownCheckboxesGroups[i].querySelector('.checkbox-main');
+                let groupCheckboxes = reviewsFiltersDropDownCheckboxesGroups[i].querySelectorAll('.checkbox-group-item');
+
+                checkboxMain.checked = false;
+                
+                for (let item of groupCheckboxes) {
+                    item.checked = false;
+                }
+
+                for (let item of reviewsFiltersDropDownItems) {
+                    if (item.classList.contains('choosed')) {
+                        item.classList.remove('choosed');
+                    }
+                }
+
+                for (let item of reviewsFiltersItemClinics) {
+                    item.classList.remove('active');
+                }
+
+                reviewsFiltersItemClinics[0].classList.add('active');
+            }
+        });
+
+    };
+
+
+    /************ Filters left side of reviews End  *************/
+
+    
+    /************ Filter above the list of reviews Start *************/
+
+    let filterAboveReviewsBtn = document.querySelector('.reviews__reviews-filter-btn');
+    let filterAboveReviewsDropDown = document.querySelector('.reviews__reviews-filter-dropdown');
+    let filterAboveReviewsValue = document.querySelector('.reviews__reviews-filter-btn-val');
+    let filterAboveReviewsItems = document.querySelectorAll('.reviews__reviews-filter-dropdown-item');
+    
+    if (filterAboveReviewsBtn !== null) {
+
+        for (let item of filterAboveReviewsItems) {
+            item.addEventListener('click', function() {
+                filterAboveReviewsValue.textContent = item.textContent;
+                filterAboveReviewsDropDown.classList.remove('show');
+            });
+        }
+
+        filterAboveReviewsBtn.addEventListener('click', function() {
+            filterAboveReviewsDropDown.classList.toggle('show');
+        });
+
+        document.addEventListener('click', function(e) {
+            const withinBoundaries = e.composedPath().includes(filterAboveReviewsDropDown);
+            const withinBtn = e.composedPath().includes(filterAboveReviewsBtn);
+         
+            if ( !withinBoundaries && !withinBtn ) {
+                if (filterAboveReviewsDropDown.classList.contains('show')) {
+                    filterAboveReviewsDropDown.classList.remove('show');
+                }
+            }
+        })
+
+    }
+
+    /************ Filter above the list of reviews End *************/
+
+
+
+
+    /************ Paginations for page Reviews Start *************/ 
+
+    let paginationBtnLeft = document.querySelector('.reviews__reviews-pagination-left');
+    let paginationBtnRight = document.querySelector('.reviews__reviews-pagination-right');
+    let paginationItems = document.querySelectorAll('.reviews__reviews-pagination-item');
+    
+    if (paginationBtnLeft !== null) {
+        paginationBtnLeft.addEventListener('click', function() {
+            if (!paginationBtnLeft.classList.contains('active')) {
+                paginationBtnRight.classList.remove('active');
+                paginationBtnLeft.classList.add('active');
+            }
+        });
+    }
+
+    if (paginationBtnRight !== null) {
+        paginationBtnRight.addEventListener('click', function() {
+            if (!paginationBtnRight.classList.contains('active')) {
+                paginationBtnLeft.classList.remove('active');
+                paginationBtnRight.classList.add('active');
+            }
+        });
+    }
+
+    if (paginationItems[0] !== null) {
+        for (let i = 0; i < paginationItems.length; i++) {
+            paginationItems[i].addEventListener('click', function() {
+                if (!paginationItems[i].classList.contains('active')) {
+                    for (let item of paginationItems) {
+                        item.classList.remove('active');
+                    }
+                    paginationItems[i].classList.add('active');
+                }
+            });
+        }
+    }
+
+    /************ Paginations End *************/
+
+    /************ Reviews Resource Overlay Start *************/
+
+    let reviewsResourcesBtnAll = document.querySelector('.reviews__resources-btn-all');
+    let resourcesPanel = document.querySelector('.resources-panel');
+    let resourcesPanelBody = document.querySelector('.resources-panel__body');
+    let resourcesPanelCloseBtn = document.querySelector('.resources-panel__close');
+
+    if (reviewsResourcesBtnAll !== null) {
+
+        reviewsResourcesBtnAll.addEventListener('click', function() {
+            resourcesPanel.classList.add('show');
+            resourcesPanelBody.classList.add('open');
+            document.body.classList.add('body-overflow');
+        });
+
+        resourcesPanelCloseBtn.addEventListener('click', function() {
+            resourcesPanelBody.classList.remove('open');
+            setTimeout(function() {
+                resourcesPanel.classList.remove('show');
+                document.body.classList.remove('body-overflow'); 
+            }, 500);
+            
+        });
+    }
+     /************ Reviews Resource Overlay End *************/
+
+    
+
  
 
 
